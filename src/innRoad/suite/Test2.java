b710558aa0fc97ae9a479bc15e3adea3fc758ff0
sudhakotni <sudha.kotni@innroad.com>
2016-06-30 13:19:28 +0530

    uploading Test file

---
+++ src/innRoad/suite/Test2.java
@@ -0,0 +1,12 @@
+package innRoad.suite;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+@SuppressWarnings("unused")
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
